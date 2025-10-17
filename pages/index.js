3c62dde61c53f000a80b3d714cba0343f3445477
Dalfeee <dalfe.ferreira@gmail.com>
2025-10-17 15:41:40 -0400

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-function Home(){
-    return <h1>Olá Mundo! Absolte cinema!</h1>
+function Home() {
+  return <h1>Olá Mundo! Absolte cinema!</h1>;
+}
+
+function test() {
+  console.log("teste");
 }
 
 export default Home;
-
-    
