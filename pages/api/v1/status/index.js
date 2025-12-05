8ac71aad79134dc7c708329878e5bffe931f4773
Dalfeee <dalfe.ferreira@gmail.com>
2025-12-05 17:23:41 -0400

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,43 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "São zika" });
+  // Updated
+  const updatedAt = new Date().toISOString();
+
+  // version postgres
+  const DatabaseVersionResult = await database.query("SHOW server_version;");
+  const DatabaseVersionValue = DatabaseVersionResult.rows[0].server_version;
+
+  // Conexões máximas
+  const MaxConectionsResult = await database.query("Show max_connections;");
+  const MaxConectionsValue = MaxConectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResults = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResults.rows[0].count;
+
+  // Conexões em uso
+  const ConnInUseResult = await database.query(
+    "SELECT count(*) FROM pg_stat_activity WHERE state = 'active';",
+  );
+  const ConnInUseValue = ConnInUseResult.rows[0].count;
+
+  // Endpoint
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: DatabaseVersionValue,
+        Max_connections: parseInt(MaxConectionsValue),
+        in_use_connections: ConnInUseValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
+const { version } = require("react");
+
 test("GET to /api/v1/status should returno 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  // Teste auto de "updated_at" da API
+  const responseBody = await response.json();
+  const pasedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(pasedUpdatedAt);
+
+  // postgres version
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  // Conexões máximas
+  expect(responseBody.dependencies.database.Max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+
+  // Conexões usadas
+  expect(responseBody.dependencies.database.in_use_connections).toEqual("1");
 });
